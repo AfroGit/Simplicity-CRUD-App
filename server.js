b6a62a6a9c9c9c17d6cc9e08786ce8e568233751
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-04-21 14:42:45 +0000

    Moved all my express handlers into the mongoclient function.

--- server.js
+++ server.js
@@ -6,22 +6,37 @@ const MongoClient = require('mongodb').MongoClient
 const connectionString = "mongodb+srv://KujengA4222:CVIbkatM0ACWn7nv@cluster0.lhyk1vq.mongodb.net/?retryWrites=true&w=majority"
 
 
-MongoClient.connect(connectionString, { useUnifiedTopology: true })
+MongoClient.connect(connectionString, 
+    { 
+      useUnifiedTopology: true 
+    })
   .then(client => {
     console.log('Connected to Database')
+    const db = client.db('star-wars-quotes')
+
+    app.use(bodyParser.urlencoded({ extended: true }))
+
+    app.get('/', (req, res) => {
+  res.sendFile(__dirname + '/index.html')
+})
+
+    app.post('/quotes', (req, res) => {
+  console.log(req.body)
+})
+
   })
   .catch(error => console.error(error))
 // place body-parser before your CRUD handlers!
 
-app.use(bodyParser.urlencoded({ extended: true }))
+// app.use(bodyParser.urlencoded({ extended: true }))
 
 // app.get('/', (req, res) => {
 //   res.send('Anaweza Bwana')
 // })
 
-app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/index.html')
-})
+// app.get('/', (req, res) => {
+//   res.sendFile(__dirname + '/index.html')
+// })
 
 app.post('/quotes', (req, res) => {
   console.log(req.body)
