82f863baaf9dfbdc36d28899f5da83ab7807aa12
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-04-24 21:06:36 +0000

    We need to tell the server we’re sending JSON data by setting the Content-Type headers to application/json.
    Modern applications send JSON data to servers. They also receive JSON data back to servers. JSON stands for JavaScript Object Notation. They’re like JavaScript objects, but each property and value are written between two quotation marks.

---
+++ public/main.js
@@ -0,0 +1,20 @@
+const update = document.querySelector('#update-button')
+
+update.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'put',
+     headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      name: 'Darth Vader',
+      quote: 'I find your lack of faith disturbing.'
+    })
+    
+  })
+  .then(res => {// Send PUT Request here
+    if(res.ok) return res.json()
+  })
+  .then(response => {
+     window.location.reload(true)//reload window after every input change
+  })
+  
+})
