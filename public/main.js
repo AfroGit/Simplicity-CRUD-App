8dab15726ada1a169eae3ec2820d1ef869f7aed4
AfroGit <94635472+AfroGit@users.noreply.github.com>
2023-04-25 16:19:54 +0000

    MongoDB Collections has a method called deleteOne. It lets us remove a document from the database. It takes in two parameters: query and options.
    query works like query in findOneAndUpdate. It lets us filter the collection to the entries we’re searching for.

--- public/main.js
+++ public/main.js
@@ -1,5 +1,7 @@
 const update = document.querySelector('#update-button')
 
+const deleteButton = document.querySelector('#delete-button')
+
 update.addEventListener('click', _ => {
   fetch('/quotes', {
     method: 'put',
@@ -17,4 +19,21 @@ update.addEventListener('click', _ => {
      window.location.reload(true)//reload window after every input change
   })
   
+})
+
+
+deleteButton.addEventListener('click', _ => {
+  fetch('/quotes', {
+    method: 'delete',
+    headers: { 'Content-Type': 'application/json' },
+     body: JSON.stringify({
+      name: 'Darth Vader'
+    })
+  })
+  .then(res => {
+      if (res.ok) return res.json()
+    })
+    .then(data => {
+      window.location.reload()
+    })
 })
